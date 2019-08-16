61e68d3b14d457efa94b52ac25797c8bfa6f7e08
anorrie <anorrie@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2019-08-16 14:02:51 +0000

    Tested against all current breaker config file
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Application%20Notes/AN-020%20-%20Module%20control%20with%20config%20files/Trunk@3251 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- QisListDevices.py
+++ QisListDevices.py
@@ -16,24 +16,59 @@ For remote QIS, comment out the 'openQis()' command and specify the IP:Port in t
 
 ####################################
 '''
-from quarchpy import qisInterface, isQisRunning, startLocalQis
-import time
+from quarchpy.device import *
+from quarchpy.config_files import *
+from quarchpy.config_files import test_config_parser
 
-# Checks is QIS is running on the localhost
-if isQisRunning() == False:
-    # Start the version on QIS installed with quarchpy
-    startLocalQis()
+# Scan for devices
+allDevices = scanDevices("all")
+# Ask user to select one and connect to it
+dev_select = userSelectDevice (allDevices)    
+my_device = quarchDevice (dev_select)
 
-# Connect to the localhost QIS instance - you can also specify host='127.0.0.1' and port=9722 for remote control.
-myQis = qisInterface()
+# Find the correct config file for the connected module (breaker modules only for now)
+# We're passing the module connection here, the idn_string can be supplied instead if the module is not currently attached (simulation/demo mode)
+file = get_config_path_for_module (module_connection = my_device)
+# Parse the file to get the device capabilities
+dev_caps = parse_config_file (file)
+print ("CONFIG FILE LOCATED:")
+print (file)
+print ("")
+print ("")
 
-#small sleep to allow qis to scan for devices
-time.sleep(5)
+# Prints a list of top level capabilities that this module has (differentiating from a 'base' module)
+print ("GENERAL CAPABILITIES:")
+for key, value in dev_caps.get_general_capabilities().items():
+    print(key + " = " + value)
+print ("")
+print ("")
 
-# Request a list of all USB and LAN accessible modules
-devList = myQis.getDeviceList()
+# Print the list of signals on the module, and the capability flags for each signal
+print ("SIGNALS AVAILABLE:")
+for sig in dev_caps.get_signals():
+    print ("Name:\t" + sig.name)
+    for key, value in sig.parameters.items():
+        print("\t" + key + " = " + value)
+print ("")
+print ("")
 
-# Print the devices
-print ("\nList of devices attached to QIS:\n")
-for device in devList:
-    print (device)
+# Print out the list of signal groups, and the list of signals they control
+print ("SIGNALS GROUPS AVAILABLE:")
+for group in dev_caps.get_signal_groups():
+    print ("Name:\t" + group.name)
+    print ("\t", end='')
+    for sig in group.signals:
+        print (sig + ",", end='')
+    print ("")
+print ("")
+print ("")
+
+# Print the sources on the module, and list their capabilities
+print ("SOURCES AVAILABLE:")
+for source in dev_caps.get_sources():
+    print ("Name:\t" + source.name)
+    for key, value in source.parameters.items():
+        print("\t" + key + " = " + str(value))
+    print ("")
+print ("")
+print ("")
